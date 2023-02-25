f90263b0a5d872a16af0569d415104dc8f1ef11a
mateusvbfontes <87830682+mateusvbfontes@users.noreply.github.com>
2023-02-24 21:32:33 -0300

    Add tarefa 03

---
+++ tarefas/tarefa-aula03/js/script.js
@@ -0,0 +1,20 @@
+alert("Olá!")
+
+// const A = 10
+// const b = 20
+
+// let resultado = a + b
+// console.log(resultado)
+
+// resultado = a- b
+// console.log(resultado)
+
+
+const a = Number(prompt("Digite o valor do salário:"))
+const b = Number(prompt("Digite o valor da porcentagem de aumento"))
+
+const lista = [a,b]
+
+let resultado = a * b / 100
+
+document.write(resultado);
