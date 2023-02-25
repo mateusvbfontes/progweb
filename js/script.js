84c21da6813f747bf5249d02df88cc3129df732c
mateusvbfontes <87830682+mateusvbfontes@users.noreply.github.com>
2023-02-24 21:22:59 -0300

    add script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 alert("Olá!")
+
+// const A = 10
+// const b = 20
+
+// let resultado = a + b
+// console.log(resultado)
+
+// resultado = a- b
+// console.log(resultado)
+
+
+const a = Number(prompt("Digite o valor de A"))
+const b = Number(prompt("Digite o valor de B"))
+
+const lista = [a,b]
+
+let resultado = a + b
+console.log(resultado)
+
+resultado = a- b
+console.log(resultado)
+
+document.write(resultado);
